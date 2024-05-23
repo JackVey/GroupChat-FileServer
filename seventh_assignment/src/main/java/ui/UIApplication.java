95e56a82beaa3c47ae4abc226050255a477905c1
Mohammadreza Amini <161158007+JackVey@users.noreply.github.com>
2024-05-23 21:26:24 +0330

    set the stages' Resizeable flag to false

--- seventh_assignment/src/main/java/ui/UIApplication.java
+++ seventh_assignment/src/main/java/ui/UIApplication.java
@@ -8,15 +8,17 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class UIApplication extends Application {
+    public static Stage stage;
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(UIApplication.class.getResource("UI.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load(), 1280, 720);
+        stage.setResizable(false);
+        stage.setTitle("GroupChat");
         stage.setScene(scene);
         stage.show();
+        UIApplication.stage = stage;
     }
-
     public static void main(String[] args) {
         launch();
     }
