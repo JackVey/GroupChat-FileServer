739bede6508158e89c73a5dd969833c37b97be02
Mohammadreza Amini <161158007+JackVey@users.noreply.github.com>
2024-05-24 19:56:59 +0330

    renamed UIController.java to ChatController.java

--- seventh_assignment/src/main/java/ui/ChatController.java
+++ seventh_assignment/src/main/java/ui/ChatController.java
@@ -2,13 +2,14 @@ package ui;
 
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class UIController {
+public class ChatController implements Initializable {
     @FXML
     protected void onStartChatButtonClick() throws IOException {
         Scene scene = new Scene(FXMLLoader.load(getClass().getResource("GroupChatUI.fxml")), 1280, 720);
@@ -18,5 +19,10 @@ public class UIController {
     protected void onBackButtonClick() throws IOException {
         Scene scene = new Scene(FXMLLoader.load(getClass().getResource("mainPageUI.fxml")), 1280, 720);
         UIApplication.stage.setScene(scene);
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
     }
 }
