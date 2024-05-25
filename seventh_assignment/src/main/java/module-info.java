185d9212a8a05414809ee9cef99a918aea733302
Mohammadreza Amini <161158007+JackVey@users.noreply.github.com>
2024-05-25 20:21:13 +0330

    added json to gradle

--- seventh_assignment/src/main/java/module-info.java
+++ seventh_assignment/src/main/java/module-info.java
@@ -2,6 +2,7 @@ module seventh.assignment.main {
     requires javafx.controls;
     requires javafx.fxml;
     requires org.kordamp.bootstrapfx.core;
+    requires org.json;
 
     opens ui to javafx.fxml;
     exports ui;
