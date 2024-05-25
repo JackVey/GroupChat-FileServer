75b3171689879c17278c4b8924a1f20fe820fb5d
Mohammadreza Amini <161158007+JackVey@users.noreply.github.com>
2024-05-26 00:00:01 +0330

    implemented initializable interface to controller class DownloadPageController and an on action method onDownloadButtonClick()

--- seventh_assignment/src/main/java/ui/DownloadPageController.java
+++ seventh_assignment/src/main/java/ui/DownloadPageController.java
@@ -1,15 +1,28 @@
 package ui;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Scene;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class DownloadPageController {
+public class DownloadPageController implements Initializable {
     @FXML
     protected void onBackButtonClick() throws IOException {
         Scene scene = new Scene(FXMLLoader.load(getClass().getResource("mainPageUI.fxml")), 1280, 720);
         UIApplication.stage.setScene(scene);
+    }
+    @FXML
+    public void onDownloadButtonClick() {
+
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
     }
 }
