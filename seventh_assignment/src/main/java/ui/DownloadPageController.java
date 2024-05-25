64061d9e0eb3f3a0526e07fd378f116aba7a51b0
Mohammadreza Amini <161158007+JackVey@users.noreply.github.com>
2024-05-25 23:51:38 +0330

    added onBackButtonClick() method to the controller

--- seventh_assignment/src/main/java/ui/DownloadPageController.java
+++ seventh_assignment/src/main/java/ui/DownloadPageController.java
@@ -1,4 +1,15 @@
 package ui;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+
+import java.io.IOException;
+
 public class DownloadPageController {
+    @FXML
+    protected void onBackButtonClick() throws IOException {
+        Scene scene = new Scene(FXMLLoader.load(getClass().getResource("mainPageUI.fxml")), 1280, 720);
+        UIApplication.stage.setScene(scene);
+    }
 }
