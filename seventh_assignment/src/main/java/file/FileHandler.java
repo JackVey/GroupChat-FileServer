5b1c3453e384b92935c714368ab918f60278acd9
Mohammadreza Amini <161158007+JackVey@users.noreply.github.com>
2024-05-25 18:34:27 +0330

    created FileHandler.java to save messages

---
+++ seventh_assignment/src/main/java/file/FileHandler.java
@@ -0,0 +1,17 @@
+package file;
+
+import java.util.ArrayList;
+
+public class FileHandler {
+
+    private final String MESSAGES_FILE_ADDRESS = "File\\messages.txt";
+    public static synchronized void writeMessage(String messageToWrite){
+
+    }
+
+    public static synchronized ArrayList<String> readMessages(){
+        return null;
+    }
+
+
+}
