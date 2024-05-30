77c7b850774787e7535faaf905410107048b4248
Mohammadreza Amini <161158007+JackVey@users.noreply.github.com>
2024-05-30 18:12:04 +0330

    fixed some warnings in API

--- seventh_assignment/src/main/java/server/API.java
+++ seventh_assignment/src/main/java/server/API.java
@@ -7,17 +7,15 @@ import java.io.*;
 import java.net.Socket;
 
 public class API implements Runnable{
-    private Socket socket;
-    private DataOutputStream dataOutputStream;
-    private DataInputStream dataInputStream;
-    private BufferedReader bufferedReader;
-    private BufferedWriter bufferedWriter;
+    private final Socket socket;
+    private final DataOutputStream dataOutputStream;
+    private final BufferedReader bufferedReader;
+    private final BufferedWriter bufferedWriter;
 
     public API(Socket socket) {
         this.socket = socket;
         try {
             this.dataOutputStream = new DataOutputStream(socket.getOutputStream());
-            this.dataInputStream = new DataInputStream(socket.getInputStream());
             this.bufferedWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
             this.bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
         } catch (IOException e) {
